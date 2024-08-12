8398d1f67bb96201192f3e2f3d62cb7ba41b88ee
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-12 23:30:12 +0300

    Create Flask application and SQLAlchemy object

---
+++ yacut/__init__.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from settings import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
