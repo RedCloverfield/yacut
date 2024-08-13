6c05296d047b2f25e143aeba03943dec5e7995bc
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-13 17:38:07 +0300

    Create index_view

--- yacut/views.py
+++ yacut/views.py
@@ -1,9 +1,32 @@
 from random import choice
 from string import ascii_lowercase, ascii_uppercase, digits
 
+from flask import flash, render_template
+
+from yacut import app, db
+from .forms import URLForm
 from .models import URLMap
 
 SYMBOLS_FOR_SHORT_ID = ascii_lowercase + ascii_uppercase + digits
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    form = URLForm()
+    if form.validate_on_submit():
+        short_url = form.custom_id.data
+        if short_url:
+            if URLMap.query.filter_by(short=short_url).first():
+                flash('Предложенный вариант короткой ссылки уже существует')
+                return render_template('index.html', form=form)
+        else:
+            short_url = get_unique_short_id()
+        db.session.add(URLMap(
+            original=form.original_link.data,
+            short=short_url)
+        )
+        db.session.commit()
+    return render_template('index.html', form=form)
 
 
 def get_unique_short_id():
