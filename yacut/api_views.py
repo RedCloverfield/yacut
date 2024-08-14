d71d242301cf1a8917b3408dc8979435f8312484
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 00:18:19 +0300

    Add fields validation. Small changes

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,24 +1,42 @@
 from flask import jsonify, request, url_for
 
 from yacut import app, db
+from .error_handlers import InvalidAPIUsage
 from .models import URLMap
+from .views import get_unique_short_id
+from settings import SYMBOLS_FOR_SHORT_ID
 
 
 @app.route('/api/id/', methods=['POST'])
 def create_short_url():
     data = request.get_json(silent=True)
+    if data is None:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    if data.get('url') is None:
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+    if custom_id := data.get('custom_id'):
+        if len(custom_id) > 16:
+            raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+        for symbol in custom_id:
+            if symbol not in SYMBOLS_FOR_SHORT_ID:
+                raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+    else:
+        custom_id = get_unique_short_id()
+    if URLMap.query.filter_by(short=custom_id).first():
+        raise InvalidAPIUsage('Предложенный вариант короткой ссылки уже существует.')
     db.session.add(
         URLMap(
             original=data.get('url'),
-            short=data.get('custom_id')
+            short=custom_id
         )
     )
     db.session.commit()
-    short_link = url_for('redirect_view', short_id=data.get('custom_id'), _external=True)
+    short_link = url_for('redirect_view', short_id=custom_id, _external=True)
     return jsonify({'url': data.get('url'), 'short_link': short_link}), 201
 
 
 @app.route('/api/id/<string:short_id>/', methods=['GET'])
 def get_original_link(short_id):
-    url = URLMap.query.filter_by(short=short_id).first().original
-    return jsonify({'url': url}), 200
+    if url := URLMap.query.filter_by(short=short_id).first():
+        return jsonify({'url': url.original}), 200
+    raise InvalidAPIUsage('Указанный id не найден', 404)
