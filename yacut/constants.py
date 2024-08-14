65670d6354f0602ce5195ce9b47ab6919c4b9635
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 00:36:39 +0300

    Create constants

---
+++ yacut/constants.py
@@ -0,0 +1,8 @@
+
+class Messages():
+    ID_ALREADY_EXISTS = 'Предложенный вариант короткой ссылки уже существует.'
+    REQUIRED_FIELD = 'Это поле обязательно для заполнения'
+    BLANK_REQUEST_BODY = 'Отсутствует тело запроса'
+    BLANK_URL_FIELD = '"url" является обязательным полем!'
+    INCORRECT_ID_FIELD_VALUE = 'Указано недопустимое имя для короткой ссылки'
+    ID_NOT_FOUND = 'Указанный id не найден'
