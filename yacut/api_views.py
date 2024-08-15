7872649326b91ecc82a04bfef7e582bb7e6665e1
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 17:28:54 +0300

    Small changes

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -15,29 +15,29 @@ def create_short_url():
         raise InvalidAPIUsage(Messages.BLANK_REQUEST_BODY)
     if data.get('url') is None:
         raise InvalidAPIUsage(Messages.BLANK_URL_FIELD)
-    if custom_id := data.get('custom_id'):
-        if len(custom_id) > 16:
+    if short := data.get('custom_id'):
+        if len(short) > 16:
             raise InvalidAPIUsage(Messages.INCORRECT_ID_FIELD_VALUE)
-        for symbol in custom_id:
+        for symbol in short:
             if symbol not in SYMBOLS_FOR_SHORT_ID:
                 raise InvalidAPIUsage(Messages.INCORRECT_ID_FIELD_VALUE)
     else:
-        custom_id = get_unique_short_id()
-    if URLMap.query.filter_by(short=custom_id).first():
+        short = get_unique_short_id()
+    if URLMap.query.filter_by(short=short).first():
         raise InvalidAPIUsage(Messages.ID_ALREADY_EXISTS)
     db.session.add(
         URLMap(
             original=data.get('url'),
-            short=custom_id
+            short=short
         )
     )
     db.session.commit()
-    short_link = url_for('redirect_view', short_id=custom_id, _external=True)
+    short_link = url_for('redirect_view', short=short, _external=True)  # изменить имя переменной
     return jsonify({'url': data.get('url'), 'short_link': short_link}), 201
 
 
-@app.route('/api/id/<string:short_id>/', methods=['GET'])
-def get_original_link(short_id):
-    if url := URLMap.query.filter_by(short=short_id).first():
+@app.route('/api/id/<string:short>/', methods=['GET'])
+def get_original_link(short):
+    if url := URLMap.query.filter_by(short=short).first():
         return jsonify({'url': url.original}), 200
     raise InvalidAPIUsage(Messages.ID_NOT_FOUND, 404)
