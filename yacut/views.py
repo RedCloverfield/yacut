fa6b9cfb03af47911244dabf989cc644fd85056b
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 17:25:40 +0300

    Small changes

--- yacut/views.py
+++ yacut/views.py
@@ -1,10 +1,9 @@
-from random import choice
+from random import choices
 
 from flask import flash, render_template, redirect
 
-from settings import SYMBOLS_FOR_SHORT_ID
-
 from yacut import app, db
+from settings import SYMBOLS_FOR_SHORT_ID
 from .constants import Messages
 from .forms import URLForm
 from .models import URLMap
@@ -13,34 +12,33 @@ from .models import URLMap
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLForm()
-    if form.validate_on_submit():
-        if (short_id := form.custom_id.data):
-            if URLMap.query.filter_by(short=short_id).first():
-                flash(Messages.ID_ALREADY_EXISTS)
-                return render_template('index.html', form=form)
-        else:
-            short_id = get_unique_short_id()
-        db.session.add(
-            URLMap(
-                original=form.original_link.data,
-                short=short_id
-            )
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form)
+    if (short := form.custom_id.data):
+        if URLMap.query.filter_by(short=short).first():
+            flash(Messages.ID_ALREADY_EXISTS)
+            return render_template('index.html', form=form)
+    else:
+        short = get_unique_short_id()
+    db.session.add(
+        URLMap(
+            original=form.original_link.data,
+            short=short
         )
-        db.session.commit()
-        return render_template('index.html', form=form, short_id=short_id)
-    return render_template('index.html', form=form)
+    )
+    db.session.commit()
+    return render_template('index.html', form=form, short=short)
 
 
-@app.route('/<string:short_id>', methods=['GET'])
-def redirect_view(short_id):
-    url = URLMap.query.filter_by(short=short_id).first_or_404().original
+
+@app.route('/<string:short>', methods=['GET'])
+def redirect_view(short):
+    url = URLMap.query.filter_by(short=short).first_or_404().original
     return redirect(url, code=302)
 
 
 def get_unique_short_id():
-    short_id = ''
-    for i in range(6):
-        short_id += choice(SYMBOLS_FOR_SHORT_ID)
-    if URLMap.query.filter_by(short=short_id).first():
+    short = ''.join(choices(SYMBOLS_FOR_SHORT_ID, k=6))
+    if URLMap.query.filter_by(short=short).first():
         get_unique_short_id()
-    return short_id
+    return short
