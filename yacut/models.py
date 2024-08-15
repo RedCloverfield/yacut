5bfdd3835f750f6771c4b37dd0c55a29cc8527d5
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 17:29:45 +0300

    Use constants. Fix default timestamp value

--- yacut/models.py
+++ yacut/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 from yacut import db
+from .constants import ORIGINAL_LINK_LENGTH, SHORT_ID_LENGTH
 
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), unique=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    original = db.Column(db.String(ORIGINAL_LINK_LENGTH), nullable=False)
+    short = db.Column(db.String(SHORT_ID_LENGTH), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
