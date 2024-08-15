3b3681f4bbd6d4a3534fe6c8aefae6b9078d5f4b
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-15 17:28:35 +0300

    Add length validation. Create constants

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,15 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import URLField, SubmitField
 from wtforms.validators import DataRequired, Length, Optional
 
-from .constants import Messages
+from .constants import Messages, ORIGINAL_LINK_LENGTH, SHORT_ID_LENGTH
+
+LONG_LINK = 'Длинная ссылка'
+SHORT_LINK = 'Ваш вариант короткой ссылки'
+CREATE_OBJECT = 'Создать'
 
 
 class URLForm(FlaskForm):
     original_link = URLField(
-        label='Длинная ссылка',
-        validators=[DataRequired(message=Messages.REQUIRED_FIELD)])
+        label=LONG_LINK,
+        validators=[
+            DataRequired(message=Messages.REQUIRED_FIELD),
+            Length(max=ORIGINAL_LINK_LENGTH)
+        ])
     custom_id = URLField(
-        label='Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        label=SHORT_LINK,
+        validators=[Length(max=SHORT_ID_LENGTH), Optional()]
     )
-    submit = SubmitField(label='Создать')
+    submit = SubmitField(label=CREATE_OBJECT)
